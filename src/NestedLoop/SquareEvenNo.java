536b49a7a7fa6f669b9ed38d4e896de196e8b6a3
Sarvesh <sarvesh.wayangankar@gmail.com>
2026-03-19 18:40:09 +0530

    Nested loop problems

---
+++ src/NestedLoop/SquareEvenNo.java
@@ -0,0 +1,23 @@
+package NestedLoop;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.stream.Collectors.*;
+
+public class SquareEvenNo {
+	public static void main(String[] args) {
+		
+		List<Integer> list = List.of(1,2,2,3,3,4,4,5,5,6,6);
+		
+		List<Integer> even = list.stream()
+				.filter(n -> n%2==0)
+				.distinct()
+				.map(n -> n*n)
+				.collect(Collectors.toList());
+		
+//		List<Integer> res=	even.stream()
+//						.map(n->n*n)
+//						.collect(Collectors.toList());
+		System.out.println(even);
+	}
+}
